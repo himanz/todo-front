8b6859003fee9f120bb5f7f057742161c35f758c
Jonathan Chang <jonoschang7@gmail.com>
2014-10-17 17:15:09 -0400

    user registration works

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -2,26 +2,26 @@
 
 angular.module('todo')
   .controller('AuthCtrl', ['$scope', '$auth', function ($scope, $auth) {
-    $scope.handleRegBtnClick = function() {
-      $auth.submitRegistration($scope.registrationForm)
-        .then(function(resp) { 
-          // handle success response
-          return resp;
-        })
-        .catch(function(resp) { 
-          // handle error response
-          if(resp.status === 401) {
-            console.log('ERROR');
-          }
-        });
-    };
-    $scope.handleLoginBtnClick = function() {
-      $auth.submitLogin($scope.loginForm)       
-        .then(function(resp) { 
-          // handle success response
-        })
-        .catch(function(resp) { 
-          // handle error response
-        });  
-    };
+    // $scope.handleRegBtnClick = function() {
+    //   $auth.submitRegistration($scope.registrationForm)
+    //     .then(function(resp) { 
+    //       // handle success response
+    //       return resp;
+    //     })
+    //     .catch(function(resp) { 
+    //       // handle error response
+    //       if(resp.status === 401) {
+    //         console.log('ERROR');
+    //       }
+    //     });
+    // };
+    // $scope.handleLoginBtnClick = function() {
+    //   $auth.submitLogin($scope.loginForm)       
+    //     .then(function(resp) { 
+    //       // handle success response
+    //     })
+    //     .catch(function(resp) { 
+    //       // handle error response
+    //     });  
+    // };
   }]);
